a4a7c1d085df6bfeccc94cdee0f6f3077d9c9c25
guowenjialy <guowenjialy@163.com>
2017-07-28 15:42:06 +0800

    add print WaHHHH

--- src/com/gwj/hello/HelloWorld.java
+++ src/com/gwj/hello/HelloWorld.java
@@ -6,5 +6,6 @@ package com.gwj.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World!");
+        System.out.print("WaHHHH");
     }
 }
