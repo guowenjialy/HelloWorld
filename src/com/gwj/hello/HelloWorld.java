908fde1cb399b233bfb880f36e1d469dfb2c083a
guowenjialy <guowenjialy@163.com>
2017-07-28 15:37:34 +0800

    Initial commit

---
+++ src/com/gwj/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.gwj.hello;
+
+/**
+ * Created by guowe on 2017/7/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
